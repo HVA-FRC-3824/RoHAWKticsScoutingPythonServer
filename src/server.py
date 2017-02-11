b205534a8169d5daeea6294808573a1b9694e365
amessing <amessing@utk.edu>
2017-02-11 17:24:28 -0500

    Added some error handling for the config file.

--- src/server.py
+++ src/server.py
@@ -6,7 +6,6 @@ import signal
 import os
 import sys
 import json
-import threading
 from threading import Event
 from looper import Looper
 
@@ -17,12 +16,6 @@ from socket_server import SocketServer
 from scout_analysis import ScoutAnalysis
 from constant import Constants
 from messenger import Messenger
-
-from data_models.match import Match
-from data_models.team_logistics import TeamLogistics
-from data_models.team_pit_data import TeamPitData
-from data_models.team_ranking_data import TeamRankingData
-from data_models.team_pick_ability import TeamPickAbility
 
 from ourlogging import setup_logging
 
@@ -42,11 +35,15 @@ class Server(Looper):
             The config json converted to a `dict`
         '''
         Looper.__init__(self)
-        event_key = kwargs.get('event_key', "")
-        logger.info("Event Key: {0:s}".format(event_key))
-        self.event_key = event_key
-        self.firebase = FirebaseCom(event_key)
-        self.tba = TheBlueAlliance(event_key)
+        self.event_key = kwargs.get('event_key', "")
+        if self.event_key == "":
+            logger.critical("No event key")
+            sys.exit()
+        logger.info("Event Key: {0:s}".format(self.event_key))
+        self.firebase = FirebaseCom(self.event_key)
+        self.tba = TheBlueAlliance(self.event_key)
+
+        # event for threading not FIRST event
         self.event = None
 
         self.aggregate = kwargs.get('aggregate', False)
@@ -56,9 +53,10 @@ class Server(Looper):
 
         self.crash_reporter = kwargs.get('crash_reporter', False)
         self.cache_firebase = kwargs.get('cache_firebase', False)
+        if self.cache_firebase:
+            self.time_between_caches = kwargs.get('time_between_caches', self.DEFAULT_TIME_BETWEEN_CACHES)
 
         self.time_between_cycles = kwargs.get('time_between_cycles', self.DEFAULT_TIME_BETWEEN_CYCLES)
-        self.time_between_caches = kwargs.get('time_between_caches', self.DEFAULT_TIME_BETWEEN_CACHES)
 
         self.set_loop_time(self.time_between_cycles)
 
@@ -98,6 +96,7 @@ class Server(Looper):
             with open(os.path.dirname(os.path.abspath(__file__)) + "/../cached/" +
                       self.event_id + "/event_extras.json") as f:
                 json_dict = json.loads(f.read())
+                # Constants is a singleton
                 Constants().team_numbers = json_dict['team_numbers']
                 Constants().number_of_matches = json_dict['number_of_matches']
                 Constants().scout_names = json_dict['scout_names']
@@ -107,12 +106,18 @@ class Server(Looper):
         self.event = None
         self.running = True
         self.on_tstart()
+        iteration = 1
         while self.running:
+            logger.info("Iteration {0:d}".format(self.iteration))
             start_time = time.time()
             self.on_tloop()
             end_time = time.time()
             delta_time = end_time - start_time
+            logger.info("Iteration Ended")
+            logger.info("Time taken: {0:f}s".format(delta_time))
+            iteration += 1
             if self.loop_time - delta_time > 0 and self.running:
+                # Event is used, so that we can stop the loop early if quitting
                 self.event = Event()
                 self.event.wait(timeout=self.loop_time - delta_time)
                 self.event = None
@@ -122,8 +127,6 @@ class Server(Looper):
            :class:`SocketServer`, and :class:`ScouterAnalysis` based on the config. Also,
            caches the firebase data.
         '''
-        self.iteration = 1
-
         # initial run cache
         if self.cache_firebase:
             self.firebase.cache()
@@ -137,13 +140,12 @@ class Server(Looper):
 
     def on_tloop(self):
         '''Runs on each iteration of the main loop and aggregates the data.'''
-        logger.info("Iteration {0:d}".format(self.iteration))
+
         if self.cache_firebase and self.time_between_caches < self.time_since_last_cache:
             self.firebase.cache()
             self.time_since_last_cache = 0
 
-        start_time = time.time()
-        if hasattr(self, 'scout_analysis'):
+        if hasattr(self, 'scout_analysis') and not self.tba.event_down():
             try:
                 self.scout_analysis.analyze_scouts()
             except:
@@ -154,7 +156,8 @@ class Server(Looper):
                         logger.error("Reporting crash")
                         try:
                             self.messenger.send_message("Server Crash!!!", traceback.format_exc())
-                        # weird exception that doesn't stop the text
+                        # weird exception that doesn't stop the text/email
+                        # TODO: figure out why
                         except:
                             pass
 
@@ -171,11 +174,12 @@ class Server(Looper):
                         logger.error("Reporting crash")
                         try:
                             self.messenger.send_message("Server Crash!!!", traceback.format_exc())
-                        # weird exception that doesn't stop the text
+                        # weird exception that doesn't stop the text/email
+                        # TODO: figure out why
                         except:
                             pass
 
-            if self.tba.event_down:
+            if self.tba.event_down():
                 if self.running:
                     logger.error("The Blue Alliance is down (possibly just for this event)")
                     self.messenger.send_message("The Blue Alliance is down (possibly just for this event)", "")
@@ -194,16 +198,7 @@ class Server(Looper):
                             except:
                                 pass
 
-        end_time = time.time()
-        time_taken = end_time - start_time
-        logger.info("Iteration Ended")
-        logger.info("Time taken: {0:f}s".format(time_taken))
-        if self.time_between_cycles - time_taken > 0:
-            self.event = threading.Event()
-            self.event.wait(timeout=self.time_between_cycles - time_taken)
-            self.event = None
         self.time_since_last_cache += self.time_between_cycles
-        self.iteration += 1
 
     def stop(self):
         '''Stops all threads'''
@@ -219,11 +214,20 @@ if __name__ == "__main__":
     ap.add_argument("-c", "--config", required=True, help="Config file with settings for the server")
     args = vars(ap.parse_args())
 
-    f = open(args['config'])
-    text = f.read()
-    j = json.loads(text)
+    config_file = args['config']
+    if not os.path.isfile(config_file):
+        logger.critical("Cannot find config file: {}".format(config_file))
 
-    server = Server(**j)
+    with open(config_file) as f:
+        text = f.read()
+
+    try:
+        config = json.loads(text)
+    except:
+        logger.critical("Json exception on config file")
+        sys.exit()
+
+    server = Server(**config)
 
     def signal_handler(signal, frame):
         '''Catches control-c and cleanly shuts down the server'''
