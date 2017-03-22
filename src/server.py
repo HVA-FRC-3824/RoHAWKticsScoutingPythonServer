51c15dfc18cd4aded6ab7225fd83584fa9e4ed39
amessing <amessing@utk.edu>
2017-03-22 14:00:00 -0400

    Setup threaded server

--- src/server.py
+++ src/server.py
@@ -6,8 +6,10 @@ import sys
 import json
 import subprocess
 import re
+import platform
+from threading import Thread
 
-from socketserver import TCPServer
+from socketserver import TCPServer, ThreadingMixIn
 
 from the_blue_alliance import TheBlueAlliance
 from messenger import Messenger
@@ -19,6 +21,10 @@ from ourlogging import setup_logging
 
 setup_logging(__file__)
 logger = logging.getLogger(__name__)
+
+
+class ThreadedTCPServer(ThreadingMixIn, TCPServer):
+    pass
 
 
 class Server:
@@ -47,13 +53,16 @@ class Server:
 
         self.setup_adb_bridge()
 
-        self.socket_server = TCPServer(('localhost', 38240), SocketHandler)
+        self.socket_server = ThreadedTCPServer(('localhost', 38240), SocketHandler)
 
     def setup_adb_bridge(self):
         '''Sets up reverse port forward for attached android device running DatabaseRelay via the
            android debug bridge (adb)
         '''
-        self.adb = os.path.dirname(os.path.abspath(__file__)) + "/../adb"
+        if platform.system() == 'Darwin':
+            self.adb = '/Users/akmessing1/Library/Android/sdk/platform-tools/adb'
+        else:
+            self.adb = os.path.dirname(os.path.abspath(__file__)) + "/../adb"
         dir = os.path.dirname(__file__)
         if not os.path.exists(os.path.join(dir, self.adb)):
             logger.error("adb is not compiled")
@@ -71,22 +80,21 @@ class Server:
 
         # set reverse port forward for android device running DatabaseRelay
         # device = ''
+        port = 38240
         for device in devices:
             logger.info("Reverse port forwarding {0:s}".format(device))
-            subprocess.call([self.adb, "-s", device, "reverse", "tcp:{0:d}".format(self.port),
-                             "tcp:{0:d}".format(self.port)])
+            subprocess.call([self.adb, "-s", device, "reverse", "tcp:{0:d}".format(port),
+                             "tcp:{0:d}".format(port)])
 
     def start(self):
         '''Starts the main thread loop'''
         self.led_manager.start_up_complete()
-        self.event = None
-        self.running = True
-        while self.running:
-            self.socket_server.handle_request()
+        self.socket_server.serve_forever()
 
     def stop(self):
         '''Stops all threads'''
-        self.running = False
+        self.socket_server.shutdown()
+        self.socket_server.server_close()
         self.led_manager.stop()
 
 
--- src/socket_handler.py
+++ src/socket_handler.py
@@ -2,7 +2,7 @@ import json
 import time
 import logging
 from socketserver import StreamRequestHandler
-import urlopen
+from urllib.request import urlopen
 
 from the_blue_alliance import TheBlueAlliance
 from scout_analysis import ScoutAnalysis
@@ -15,7 +15,6 @@ logger = logging.getLogger(__name__)
 
 
 class SocketHandler(StreamRequestHandler):
-
     partial_match_updates = {}
     partial_match_time = {}
 
@@ -26,28 +25,66 @@ class SocketHandler(StreamRequestHandler):
     led_manager = LedManager()
 
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        message = self.rfile.readline().strip()
-        data = json.loads(message)
-        if data['type'] == 'heartbeat':
-            self.wfile.write('{"type": "heartbeat", "message": ""}')
-        elif data['type'] == 'match':
-            match_number = data['message']['match_number']
-            team_number = data['message']['team_number']
+        logger.info("New client: {}".format(self.client_address[0]))
+        for line in self.rfile:
+            message = str(line, 'utf-8').strip()
+            logger.info("Message Received: {}".format(message))
+            try:
+                data = json.loads(message)
+            except:
+                logger.warning("{} not jsonable".format(message))
+                continue
 
-            if match_number not in self.partial_match_updates:
-                self.partial_match_updates[match_number] = []
+            if data['type'] == 'heartbeat':
+                response = {}
+                response['type'] = "heartbeat"
+                response['data'] = {}
 
-            if team_number not in self.partial_match_updates[match_number]:
-                self.partial_match_updates[match_number].append(team_number)
-                self.partial_match_time[match_number] = time.time()
+                response_text = json.dumps(response) + "\n"
+                logger.info("Response: {}".format(response_text))
+                self.wfile.write(response_text.encode('utf-8'))
+            elif data['type'] == 'match':
+                match_number = data['data']['match_number']
+                team_number = data['data']['team_number']
 
-            if len(self.partial_match_updates[match_number]) >= 6:
-                del self.partial_match_time[match_number]
-                if not self.scout_analysis.analyze(match_number):
+                if match_number not in self.partial_match_updates:
+                    self.partial_match_updates[match_number] = []
+
+                if team_number not in self.partial_match_updates[match_number]:
+                    self.partial_match_updates[match_number].append(team_number)
+                    self.partial_match_time[match_number] = time.time()
+
+                if len(self.partial_match_updates[match_number]) >= 6:
+                    del self.partial_match_time[match_number]
+                    if not self.scout_analysis.analyze(match_number):
+                        if self.tba.event_down():
+                            try:
+                                # try google
+                                urlopen('http://216.58.192.142', timeout=1)
+                                self.led_manager.tba_down()
+                                logger.warning("The Blue Alliance is down (possibly just for this event)")
+                                self.messenger.send_message("The Blue Alliance is down (possibly just for this event)")
+                            except:
+                                self.led_manager.internet_connection_down()
+                                logger.warning("Internet connection is down")
+                        self.queued_matches.append((match_number, 1))
+                    else:
+                        Aggregator.match_calc(match_number)
+
+            elif data['type'] == 'super':
+                Aggregator.super_calc()
+            elif data['type'] == 'pilot':
+                match_number = data['data']['match_number']
+                Aggregator.pilot_calc(match_number)
+
+            # run through queue
+            temp = self.queued_matches
+            self.queued_matches = []
+
+            for (match_number, attempt) in temp:
+                if not self.scout_analysis.analyze(match_number) and attempt < 4:
                     if self.tba.event_down():
                         try:
-                            # try google
                             urlopen('http://216.58.192.142', timeout=1)
                             self.led_manager.tba_down()
                             logger.warning("The Blue Alliance is down (possibly just for this event)")
@@ -55,31 +92,7 @@ class SocketHandler(StreamRequestHandler):
                         except:
                             self.led_manager.internet_connection_down()
                             logger.warning("Internet connection is down")
-                    self.queued_matches.append((match_number, 1))
+                    self.queued_matches.append((match_number, attempt + 1))
                 else:
                     Aggregator.match_calc(match_number)
-
-        elif data['type'] == 'super':
-            Aggregator.super_calc()
-        elif data['type'] == 'pilot':
-            match_number = data['message']['match_number']
-            Aggregator.pilot_calc(match_number)
-
-        # run through queue
-        temp = self.queued_matches
-        self.queued_matches = []
-
-        for (match_number, attempt) in temp:
-            if not self.scout_analysis.analyze(match_number) and attempt < 4:
-                if self.tba.event_down():
-                    try:
-                        urlopen('http://216.58.192.142', timeout=1)
-                        self.led_manager.tba_down()
-                        logger.warning("The Blue Alliance is down (possibly just for this event)")
-                        self.messenger.send_message("The Blue Alliance is down (possibly just for this event)")
-                    except:
-                        self.led_manager.internet_connection_down()
-                        logger.warning("Internet connection is down")
-                self.queued_matches.append((match_number, attempt + 1))
-            else:
-                Aggregator.match_calc(match_number)
+        logger.info("Client lost: {}".format(self.client_address[0]))
